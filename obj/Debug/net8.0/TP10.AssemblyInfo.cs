2598d2c2f65c02a5c6483ec2e9c7602ee3c26c7e
BRUNO EZEQUIEL ROTH <49004085@ort>
2025-10-03 13:59:34 -0300

    pedilo kia-ben

--- obj/Debug/net8.0/TP10.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1ce2d87ba718b44e6d17ee7e05f1079b95f70e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+761f2cacc255fa67f327eb9ee817913de6bda5c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
